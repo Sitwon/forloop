8f9e07fdbf8ac438345a1b75ad48d3119b58ceca
Ben Mendis <ben.mendis@gmail.com>
2013-01-18 11:14:54 -0500

    Adding a simple while loop

--- src/Forloop.java
+++ src/Forloop.java
@@ -3,11 +3,20 @@ public class Forloop {
 
 	public static void main (String[] args) {
 		classic_for();
+		simple_while();
 	}
 
 	static void classic_for () {
 		for (int i = 0; i < MAX; ++i) {
 			System.out.println("classic_for: " + i);
+		}
+	}
+
+	static void simple_while () {
+		int i = 0;
+		while (i < MAX) {
+			System.out.println("simple_while: " + i);
+			++i;
 		}
 	}
 }
