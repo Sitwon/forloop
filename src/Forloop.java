3c1b465e64c87c07b6c74f2e62b84f5a19cebcc3
Ben Mendis <ben.mendis@gmail.com>
2013-01-18 11:11:16 -0500

    A classic for loop

--- src/Forloop.java
+++ src/Forloop.java
@@ -1,6 +1,14 @@
 public class Forloop {
+	static int MAX = 10;
+
 	public static void main (String[] args) {
-		System.out.println("Hello world.");
+		classic_for();
+	}
+
+	static void classic_for () {
+		for (int i = 0; i < MAX; ++i) {
+			System.out.println("classic_for: " + i);
+		}
 	}
 }
 
