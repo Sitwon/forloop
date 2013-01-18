20210b11c742d48358c8e4966b1f5982b0976ed7
Ben Mendis <ben.mendis@gmail.com>
2013-01-18 10:38:46 -0500

    Adding a classic for-loop

--- forloop.c
+++ forloop.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int MAX = 10;
+
+void classic_for () {
+	int i;
+	for (i = 0; i < MAX; ++i) {
+		printf("classic_for: %d\n", i);
+	}
+}
+
 int main (int argc, char **argv) {
+	classic_for();
 	return EXIT_SUCCESS;
 }
 
